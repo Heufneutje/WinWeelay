b474a9f9e4fafb898b6d76a3d3e6e611e153e3e6
Heufneutje <stefan.frijters@gmail.com>
2019-11-02 00:22:51 +0100

    Remove unused GitHub properties

--- WinWeelay.Utils/Update/GitHubReleaseAsset.cs
+++ WinWeelay.Utils/Update/GitHubReleaseAsset.cs
@@ -1,37 +1,15 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace WinWeelay.Utils
 {
+    /// <summary>
+    /// A downloadable asset attached to the release.
+    /// </summary>
     public class GitHubReleaseAsset
     {
-        [JsonProperty(PropertyName = "url")]
-        public string Url { get; set; }
-
-        [JsonProperty(PropertyName = "name")]
-        public string Name { get; set; }
-
-        [JsonProperty(PropertyName = "label")]
-        public object Label { get; set; }
-
-        [JsonProperty(PropertyName = "content_type")]
-        public string ContentType { get; set; }
-
-        [JsonProperty(PropertyName = "state")]
-        public string State { get; set; }
-
-        [JsonProperty(PropertyName = "size")]
-        public int Size { get; set; }
-
-        [JsonProperty(PropertyName = "download_count")]
-        public int DownloadCount { get; set; }
-
-        [JsonProperty(PropertyName = "created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        [JsonProperty(PropertyName = "updated_at")]
-        public DateTime UpdatedAt { get; set; }
-
+        /// <summary>
+        /// URL to download this asset.
+        /// </summary>
         [JsonProperty(PropertyName = "browser_download_url")]
         public string BrowserDownloadUrl { get; set; }
     }
