fc2d1cfe99f6fb5b70bae4188a5d4d1391e1b957
Heufneutje <stefan.frijters@gmail.com>
2020-11-16 20:50:20 +0100

    Remove broken obsolete formatting stripping helper

--- WinWeelay.Utils/FormattingUtils.cs
+++ WinWeelay.Utils/FormattingUtils.cs
@@ -25,46 +25,5 @@ namespace WinWeelay.Utils
 
             return fonts;
         }
-
-        /// <summary>
-        /// Remove all formatting codes from a WeeChat message.
-        /// </summary>
-        /// <param name="formattedString">The messages containing formatting codes.</param>
-        /// <returns>A stripped version of the given message.</returns>
-        public static string StripWeechatFormatting(string formattedString)
-        {
-            string result = string.Empty;
-
-            if (string.IsNullOrEmpty(formattedString))
-                return result;
-
-            bool isInFormatting = false;
-            bool isFirstFormattingCharacter = false;
-
-            foreach (char prefixChar in formattedString)
-            {
-                if (prefixChar == '\u0019' || prefixChar == '\u001a' || prefixChar == '\u001c')
-                {
-                    isInFormatting = true;
-                    isFirstFormattingCharacter = true;
-                }
-                else if (isInFormatting)
-                {
-                    if (char.IsDigit(prefixChar) || isFirstFormattingCharacter && prefixChar == 'F' || prefixChar == '@')
-                    {
-                        if (prefixChar == 'F')
-                            isFirstFormattingCharacter = false;
-
-                        continue;
-                    }
-                    isInFormatting = false;
-                }
-
-                if (!isInFormatting)
-                    result += prefixChar;
-            }
-
-            return result;
-        }
     }
 }
