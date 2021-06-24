e011afc095c9ba163d293b70db2373dcf7495585
Stefan Frijters <stefan.frijters@gmail.com>
2021-06-24 23:39:58 +0200

    Migrate password encryption to DPAPI

--- WinWeelay.Utils/Cipher.cs
+++ WinWeelay.Utils/Cipher.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Management;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -12,8 +9,6 @@ namespace WinWeelay.Utils
     /// </summary>
     public static class Cipher
     {
-        private const int KeySize = 256;
-
         /// <summary>
         /// Encrypt a given string.
         /// </summary>
@@ -24,52 +19,8 @@ namespace WinWeelay.Utils
             if (string.IsNullOrEmpty(plainText))
                 return null;
 
-            return Encrypt(plainText, GetUniqueID());
-        }
-
-        private static ICryptoTransform GetCryptoTransform(string passPhrase, byte[] saltStringBytes, byte[] ivStringBytes, bool encrypt)
-        {
-            const int derivationIterations = 1000;
-
-            using (Rfc2898DeriveBytes password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, derivationIterations))
-            {
-                byte[] keyBytes = password.GetBytes(KeySize / 8);
-                using (RijndaelManaged symmetricKey = new RijndaelManaged())
-                {
-                    symmetricKey.BlockSize = 256;
-                    symmetricKey.Mode = CipherMode.CBC;
-                    symmetricKey.Padding = PaddingMode.PKCS7;
-
-                    if (encrypt)
-                        return symmetricKey.CreateEncryptor(keyBytes, ivStringBytes);
-                    else
-                        return symmetricKey.CreateDecryptor(keyBytes, ivStringBytes);
-                }
-            }
-        }
-
-        private static string Encrypt(string plainText, string passPhrase)
-        {
-            byte[] saltStringBytes = Generate256BitsOfRandomEntropy();
-            byte[] ivStringBytes = Generate256BitsOfRandomEntropy();
-            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            using (ICryptoTransform encryptor = GetCryptoTransform(passPhrase, saltStringBytes, ivStringBytes, true))
-            {
-                using (MemoryStream memoryStream = new MemoryStream())
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                    {
-                        cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                        cryptoStream.FlushFinalBlock();
-                        byte[] cipherTextBytes = saltStringBytes;
-                        cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-                        cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-                        memoryStream.Close();
-                        cryptoStream.Close();
-                        return Convert.ToBase64String(cipherTextBytes);
-                    }
-                }
-            }
+            byte[] passBytes = ProtectedData.Protect(Encoding.UTF8.GetBytes(plainText), null, DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(passBytes);
         }
 
         /// <summary>
@@ -82,56 +33,15 @@ namespace WinWeelay.Utils
             if (string.IsNullOrEmpty(cipherText))
                 return null;
 
-            return Decrypt(cipherText, GetUniqueID());
-        }
-
-        private static string Decrypt(string cipherText, string passPhrase)
-        {
-            byte[] cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
-            byte[] saltStringBytes = cipherTextBytesWithSaltAndIv.Take(KeySize / 8).ToArray();
-            byte[] ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(KeySize / 8).Take(KeySize / 8).ToArray();
-            byte[] cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((KeySize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((KeySize / 8) * 2)).ToArray();
-
-            using (ICryptoTransform decryptor = GetCryptoTransform(passPhrase, saltStringBytes, ivStringBytes, false))
+            try
             {
-                using (MemoryStream memoryStream = new MemoryStream(cipherTextBytes))
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-                    {
-                        byte[] plainTextBytes = new byte[cipherTextBytes.Length];
-                        int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                        memoryStream.Close();
-                        cryptoStream.Close();
-                        return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
-                    }
-                }
+                byte[] passBytes = ProtectedData.Unprotect(Convert.FromBase64String(cipherText), null, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(passBytes);
             }
-        }
-
-        private static string GetUniqueID()
-        {
-            string cpuInfo = string.Empty;
-            ManagementClass mc = new ManagementClass("win32_processor");
-            ManagementObjectCollection moc = mc.GetInstances();
-
-            foreach (ManagementObject mo in moc)
+            catch
             {
-                if (cpuInfo == string.Empty)
-                {
-                    cpuInfo = mo.Properties["processorID"].Value.ToString();
-                    break;
-                }
+                return string.Empty;
             }
-            return cpuInfo;
-        }
-
-        private static byte[] Generate256BitsOfRandomEntropy()
-        {
-            byte[] randomBytes = new byte[32];
-            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
-                rngCsp.GetBytes(randomBytes);
-
-            return randomBytes;
         }
     }
 }
